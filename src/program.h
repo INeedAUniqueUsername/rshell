daab01c0c2091e3207006250c383e1f165e6d2ab
Alex Chen <achen115@ucr.edu>
2018-11-04 14:11:45 -0800

    add boost

--- src/program.h
+++ src/program.h
@@ -15,6 +15,35 @@ char* getline_c() {
 	const char* line_c = line.c_str();
 	strcpy(result, line_c);
 }
+
+void printInfo() {
+	//Get the login. If we fail, then don't provide info
+	char *login = getlogin();
+	if(login) {
+		cout << '[';
+		cout << login;
+		
+		//Now get the hostname. If we fail, then don't provide info
+		char *hostname = new char[HOST_NAME_MAX];
+		if(!gethostname(hostname, HOST_NAME_MAX)) {
+			cout << "@";
+			cout << hostname;
+			
+			//Now get the current working directory.
+			char *cwd = new char[PATH_MAX];
+			if(getcwd(cwd, PATH_MAX)) {
+				cout << " " << cwd;
+				
+				//If we got all the way here, then we have printed the info
+			}
+			delete cwd;
+		}
+		delete hostname;
+		
+		cout << ']';
+	}
+	delete login;
+}
 class Program {
 	private:
 		bool active;
@@ -24,44 +53,16 @@ class Program {
 		}
 		void run() {
 			
-			string info = "";
-			{
-				//Get the login. If we fail, then don't provide info
-				char *login = getlogin();
-				if(!login) {
-					login = (char*)"";
-				}
-				
-				//Get the hostname. If we fail, then don't provide info
-				char *hostname = new char[HOST_NAME_MAX];
-				if(gethostname(hostname, HOST_NAME_MAX)) {
-					delete hostname;
-					hostname = (char*)"";
-				}
-				if(login && hostname) {
-					info += login;
-					info += "@";
-					info += hostname;
-				}
-			}
-			
 			//Run until we get a close() call
 			while(true) {
-				if(info) {
-					cout << "[" << info;
-					char *cwd = new char[PATH_MAX];
-					if(getcwd(cwd, PATH_MAX)) {
-						cout << " " << cwd;
-					}
-					cout << "]";
-					delete cwd;
-				}
+				printInfo();
 				cout << "$ ";
 				
 				string line;
 				cin >> line;
 				//Delete everything past the comment
 				line = line.substr(0, line.find("#", 0));
+				
 				cout << "Actual: " << line << endl;
 			}
 			
