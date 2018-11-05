f47d2be78ad8cbbd5d8e592743bbc5f8f39019e8
Alex Chen <achen115@ucr.edu>
2018-11-04 16:34:41 -0800

    add tokenization

---
+++ src/program.cpp
@@ -0,0 +1,86 @@
+#include "program.h"
+#include "operation.h"
+#include<stdio.h>
+#include<string.h>
+#include<boost/tokenizer.hpp>
+#include<boost/algorithm/string/trim.hpp>
+#include<string>
+#include<vector>
+using namespace boost;
+
+void Program::run() {
+	Reader reader(this);
+	//Run until we get a close() call
+	while(true) {
+		printInfo();
+		out << "$ ";
+		
+		string line;
+		getline(in, line);
+		//Delete everything past the comment
+		line = line.substr(0, line.find("#", 0));
+		
+		/*
+		auto startIndex = 0;
+		auto endIndex = line.find(";", startIndex);
+		while(endIndex != string::npos) {
+			const string statement = line.substr(startIndex, endIndex);
+			reader.read(statement);
+			
+			startIndex = endIndex + 1;
+			endIndex = line.find(";", startIndex);
+			
+			cout << "semicolon" << endl;
+			if(endIndex == string::npos) {
+				break;
+			}
+			
+		}
+		const string statement = line.substr(startIndex, endIndex);
+		reader.read(statement);
+		*/
+		
+		
+		char_delimiters_separator<char> delimiters(false, "", ";");
+		tokenizer<char_delimiters_separator<char>> t(line, delimiters);
+		for(tokenizer<>::iterator i = t.begin(); i != t.end(); ++i) {
+			string s = *i;
+			trim(s);
+			reader.read(s);
+		}
+		
+	}
+}
+
+void Reader::read(const string& statement) {
+	vector<Operation> operations;
+	vector<Connector> connectors;
+	vector<string> parameters;
+	char_delimiters_separator<char> delimiters(false, "", " ");
+	tokenizer<char_delimiters_separator<char>> t(line, delimiters);
+	for(tokenizer<>::iterator i = t.begin(); i != t.end(); ++i) {
+		string s = *i;
+		
+		bool connector = false;
+		if(s == "&&") {
+			connector = true;
+			operations.push_back(createOperation(parameters));
+			connectors.push_back(Success());
+		} else if(s == "||") {
+			connector = true;
+			operations.push_back(createOperation(parameters));
+			connectors.push_back(Failure());
+		}
+		
+		if(connector && connectors.size() > operations.size()) {
+			//Invalid statement
+		}
+	}
+}
+Operation Reader::createOperation(const vector<string>& parameters) {
+	if(parameters.at(0) == "exit") {
+		return Exit(parent);
+	} else {
+		return Command(parameters);
+	}
+}
