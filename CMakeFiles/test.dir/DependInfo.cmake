974ced0f74214d56dd022d054a53654a05e716ac
Kyle Tran <kylezero1@gmail.com>
2018-11-11 14:36:07 -0800

    Add test for Command success / failure Add test for Chain Update CMakeLists.txt

---
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/csmajs/ktran067/assignment-1-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/src/operation.cpp" "/home/csmajs/ktran067/assignment-1-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/CMakeFiles/test.dir/src/operation.cpp.o"
+  "/home/csmajs/ktran067/assignment-1-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/src/program.cpp" "/home/csmajs/ktran067/assignment-1-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/CMakeFiles/test.dir/src/program.cpp.o"
+  "/home/csmajs/ktran067/assignment-1-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/src/test.cpp" "/home/csmajs/ktran067/assignment-1-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/CMakeFiles/test.dir/src/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "gtest_disable_pthreads=ON"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "googletest/googletest/include"
+  "googletest/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/csmajs/ktran067/assignment-1-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/googletest/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/test.cpp
@@ -0,0 +1,99 @@
+#include "operation.h"
+#include "program.h"
+#include "reader.h"
+
+#include "gtest/gtest.h"
+
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+TEST(CommandTest, EmptyString) {
+	vector<string> t;
+	Command test(t);
+	
+	EXPECT_EQ(false, test.execute());	
+}
+TEST(CommandTest, SuccessTest) {
+	vector<string> t;
+	t.push_back("ls");
+	t.push_back("-a");
+	Command test(t);
+	
+	EXPECT_EQ(true, test.execute());
+}
+TEST(CommandTest, NotBINProgram) {
+	vector<string> t;
+	t.push_back("test");
+	
+	Command test(t);
+	
+	EXPECT_EQ(false, test.execute());
+		
+	
+}
+TEST(CommandTest, IncompatibleArguments) {
+	vector<string> t;
+	t.push_back("ls");
+	t.push_back("@");
+	
+	Command test(t);
+
+	EXPECT_EQ(false, test.execute());
+	
+
+}
+
+TEST(CommandTest, ExecuteTest) {
+	vector<string> t;
+	t.push_back("echo");
+	t.push_back("hello world");
+
+	Command test(t);
+
+	testing::internal::CaptureStdout();
+	test.execute();
+	//test.print(cout);
+	string output = testing::internal::GetCapturedStdout();
+	
+	//cout << output;
+
+	EXPECT_EQ("hello world\n", output);
+}
+TEST(ChainTest, SuccessEchoTest) {
+	vector<Operation*> operations;
+	vector<Connector*> connectors;
+
+	vector<string> arguments0;
+	vector<string> arguments1;
+	
+
+	arguments0.push_back("echo");
+	arguments0.push_back("t0");
+	arguments1.push_back("echo");
+	arguments1.push_back("t1");
+	
+	Operation* t0 = new Command(arguments0);
+	Operation* t1 = new Command(arguments1);
+
+	Connector* c0 = new Success();
+
+	operations.push_back(t0);
+	operations.push_back(t1);
+
+	connectors.push_back(c0);	
+	
+	Operation *op = new Chain(operations, connectors);	
+	testing::internal::CaptureStdout();
+	op->execute();
+	string output = testing::internal::GetCapturedStdout();
+	
+	EXPECT_EQ("t0\nt1\n", output);
+}
+
+int main (int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
